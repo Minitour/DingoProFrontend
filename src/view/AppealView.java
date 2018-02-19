2a788499b7592303006d3c76c2f5275db4736d5c
Tony Zaitoun <tony.z.1711@gmail.com>
2018-02-19 19:50:33 +0200

    view appeal

--- src/view/AppealView.java
+++ src/view/AppealView.java
@@ -1,9 +1,9 @@
 package view;
 
-import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import model.Appeal;
 import ui.UIView;
 
@@ -11,8 +11,12 @@ import ui.UIView;
  * Created by Antonio Zaitoun on 14/01/2018.
  */
 public class AppealView extends UIView {
+
     @FXML
-    private JFXTextField idField;
+    private TextField userId;
+
+    @FXML
+    private TextField idField;
 
     @FXML
     private TextArea reason;
@@ -21,16 +25,29 @@ public class AppealView extends UIView {
     private Label date;
 
     public AppealView(){
-        super("/resources/xml/appeal_create.fxml");
+        super("/resources/xml/form_appeal.fxml");
     }
 
     public AppealView(Appeal appeal){
         this();
 
         //TODO: complete setup
+        int id = appeal.getDefendant().getId();
+        String reportId = appeal.getReport().getAlphaNum();
+        String reason = appeal.getReason();
+        String date = appeal.getAppealDate().toLocaleString();
+
+        userId.setText(id+"");
+        idField.setText(reportId);
+        this.reason.setText(reason);
+        this.date.setText(date);
+
+        userId.setEditable(false);
+        this.reason.setEditable(false);
+        this.idField.setEditable(false);
     }
 
-    public JFXTextField getIdField() {
+    public TextField getIdField() {
         return idField;
     }
 
